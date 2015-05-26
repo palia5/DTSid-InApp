42cc451f9aa97f395b279bc56f74e88317035b10
DriesVanItterbeeck <driesvi@hotmail.com>
2015-05-26 13:42:24 +0200

    DRIES WAS HERE

--- app/src/main/java/be/ehb/dtsid_inapp/MainActivity.java
+++ app/src/main/java/be/ehb/dtsid_inapp/MainActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DRIES WAS HERE
     }
 
     @Override
