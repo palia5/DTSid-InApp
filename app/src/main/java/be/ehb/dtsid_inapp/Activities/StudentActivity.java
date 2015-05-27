762afb4fb7b02995fffdfa646bc1cf1f1b858b75
DriesVanItterbeeck <driesvi@hotmail.com>
2015-05-27 14:21:11 +0200

    Added framework of Activities and Fragments

---
+++ app/src/main/java/be/ehb/dtsid_inapp/Activities/StudentActivity.java
@@ -0,0 +1,16 @@
+package be.ehb.dtsid_inapp.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import be.ehb.dtsid_inapp.R;
+
+public class StudentActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
