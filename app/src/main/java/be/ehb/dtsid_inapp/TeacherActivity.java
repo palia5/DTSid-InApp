cdd5d7d439cc67d371c4a021a2e8d5c2dac7f6b7
DriesVanItterbeeck <driesvi@hotmail.com>
2015-05-27 10:21:57 +0200

    Start here

---
+++ app/src/main/java/be/ehb/dtsid_inapp/TeacherActivity.java
@@ -0,0 +1,13 @@
+package be.ehb.dtsid_inapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+    }
+}
