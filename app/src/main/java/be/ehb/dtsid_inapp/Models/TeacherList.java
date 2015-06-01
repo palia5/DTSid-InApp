0a47898fe997f3d39bf955a870d4911d22582ec4
DorotheeB <dorotheebrouwers@telenet.be>
2015-06-01 11:48:15 +0200

    Aangepaste POJO's Dorothee

--- app/src/main/java/be/ehb/dtsid_inapp/Models/TeacherList.java
+++ app/src/main/java/be/ehb/dtsid_inapp/Models/TeacherList.java
@@ -1,7 +1,28 @@
 package be.ehb.dtsid_inapp.Models;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by doortje on 1/06/15.
  */
-public class TeacherList {
+public class TeacherList implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5517300947491552916L;
+    private List<Teacher> teachers;
+
+    public TeacherList() {
+        super();
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
 }
