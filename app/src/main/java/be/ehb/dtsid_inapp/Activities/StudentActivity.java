3fb49f72082486914c80cc3928700b585eb7f460
DriesVanItterbeeck <driesvi@hotmail.com>
2015-06-04 12:39:25 +0200

    StudentActivity heeft 2 wisselende fragments

--- app/src/main/java/be/ehb/dtsid_inapp/Activities/StudentActivity.java
+++ app/src/main/java/be/ehb/dtsid_inapp/Activities/StudentActivity.java
@@ -1,36 +1,75 @@
 package be.ehb.dtsid_inapp.Activities;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import be.ehb.dtsid_inapp.R;
+import be.ehb.dtsid_inapp.StudentFragments.PhotoGallery;
 import be.ehb.dtsid_inapp.StudentFragments.StudentRegistration;
-import be.ehb.dtsid_inapp.TeacherFragments.Options;
-import be.ehb.dtsid_inapp.TeacherFragments.TeacherLogin;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity
+{
+    private Boolean isInMainScreen = true;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.fragm_left_registration, new StudentRegistration());
+        ft.add(R.id.fragm_right_images, new PhotoGallery());
+        ft.commit();
     }
 
-    public void goToNextFragment(View v) {
-        Button goToButton = (Button) v;
-
-        if (goToButton.getId() == R.id.btn_student_registreren) {
-            getFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.teacherContainer, new StudentRegistration())
-                    .commit();
+    @Override
+    public void onBackPressed()
+    {
+        if(isInMainScreen)
+            super.onBackPressed();
+        else
+        {
+            changeWeightOfFragments(50, 50);
+            isInMainScreen = true;
         }
-        /*else if (goToButton.getId() == R.id.btn_teacher_login) {
-            getFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.teacherContainer, new Options())
-                    .commit();
-        } */
+    }
+
+    public void leftTouched()
+    {
+        isInMainScreen = false;
+        changeWeightOfFragments(100, 0);
+    }
+
+    public void rightTouched()
+    {
+        isInMainScreen = false;
+        changeWeightOfFragments(0, 100);
+    }
+
+    private void changeWeightOfFragments(float weightLeftFragment, float weigthRightFragment)
+    {
+        //Set registration weight
+        FrameLayout flRegistration = (FrameLayout) findViewById(R.id.fragm_left_registration);
+        LinearLayout.LayoutParams lpRegistration = new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                weightLeftFragment);
+        flRegistration.setLayoutParams(lpRegistration);
+
+        //Set images weight
+        FrameLayout flImages = (FrameLayout) findViewById(R.id.fragm_right_images);
+        LinearLayout.LayoutParams lpImages = new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                weigthRightFragment);
+        flImages.setLayoutParams(lpImages);
     }
 }
--- app/src/main/java/be/ehb/dtsid_inapp/StudentFragments/StudentRegistration.java
+++ app/src/main/java/be/ehb/dtsid_inapp/StudentFragments/StudentRegistration.java
@@ -1,18 +1,24 @@
 package be.ehb.dtsid_inapp.StudentFragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
+import be.ehb.dtsid_inapp.Activities.StudentActivity;
 import be.ehb.dtsid_inapp.R;
 
 public class StudentRegistration extends Fragment
@@ -24,13 +30,24 @@ public class StudentRegistration extends Fragment
     EditText huisNummerET;
     EditText postcodeET;
     Spinner gemeenteSP;
+    StudentActivity activity;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
         View v = inflater.inflate(R.layout.fragment_student_registration1_2, null);
+        activity = (StudentActivity) this.getActivity();
+
+        v.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                activity.leftTouched();
+                return true;
+            }
+        });
 
         emailET = (EditText) v.findViewById(R.id.et_email_subscription1);
         naamET = (EditText) v.findViewById(R.id.et_naam_subscription1);
