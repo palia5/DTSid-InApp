c8fd639b83f9fc769b7828b98eee14676005b4cc
DriesVanItterbeeck <driesvi@hotmail.com>
2015-06-09 13:17:21 +0200

    Preferences updated

--- app/src/main/java/be/ehb/dtsid_inapp/TeacherFragments/OptionsPreferences.java
+++ app/src/main/java/be/ehb/dtsid_inapp/TeacherFragments/OptionsPreferences.java
@@ -1,22 +1,16 @@
 package be.ehb.dtsid_inapp.TeacherFragments;
 
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceFragment;
 
 import be.ehb.dtsid_inapp.R;
 
-public class OptionsPreferences extends Fragment
+public class OptionsPreferences extends PreferenceFragment
 {
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
-        View v = inflater.inflate(R.layout.fragment_options_preferences, null);
-
-        return v;
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
