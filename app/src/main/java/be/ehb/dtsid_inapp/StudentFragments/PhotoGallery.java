45a82703516b9673f63d1f4131b8e0373a34f6cc
Kristof <michiels.kristof1903@gmail.com>
2015-06-11 16:29:03 +0200

    images doorlopen nu (async task) maar stoppen bij de laatste, GetImagesJSONTask toegevoegd

--- app/src/main/java/be/ehb/dtsid_inapp/StudentFragments/PhotoGallery.java
+++ app/src/main/java/be/ehb/dtsid_inapp/StudentFragments/PhotoGallery.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -22,6 +23,10 @@ import be.ehb.dtsid_inapp.Models.ZoomOutPageTransformer;
 import be.ehb.dtsid_inapp.R;
 
 public class PhotoGallery extends Fragment {
+
+    ViewPager myPager;
+    ImagePagerAdapter myImagePagerAdapter;
+
     public PhotoGallery() {
         super();
     }
@@ -34,12 +39,17 @@ public class PhotoGallery extends Fragment {
 
         int images[] = {R.drawable.the_queen, R.drawable.the_queen_amused, R.drawable.the_queen_owyeah};
 
-        ViewPager myPager = (ViewPager) v.findViewById(R.id.viewpager);
-        myPager.setAdapter(new ImagePagerAdapter(images, getActivity()));
+        myPager = (ViewPager) v.findViewById(R.id.viewpager);
+        myImagePagerAdapter = new ImagePagerAdapter(images, getActivity());
+        myPager.setAdapter(myImagePagerAdapter);
         myPager.setPageTransformer(true, new ZoomOutPageTransformer());
-        myPager.setCurrentItem(0);
 
-        //myPager.onTouchEvent()
+        ImageTimer timer = new ImageTimer(myPager, images);
+        //timer.restartTimer();
+        timer.execute();
+        //myPager.setCurrentItem(0);
+
+        //myPager.onTouchEvent(((StudentActivity) getActivity()).rightTouched())
 
         v.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -64,24 +74,72 @@ public class PhotoGallery extends Fragment {
         return null;
     }
 
+    private void nextImage(){
+        myPager.setCurrentItem(myPager.getCurrentItem() + 1, true);
+    }
+
     private class ImageTimer extends AsyncTask{
+        boolean timerStatus;
+        ViewPager mPager;
+        int images[];
+        int lengt;
 
-        public ImageTimer(){
+        public ImageTimer(ViewPager myPager, int images[]){
+            this.mPager = myPager;
+            this.images = images;
+            lengt = images.length;
+        }
 
+        @Override
+        protected void onProgressUpdate(Object[] values) {
+            super.onProgressUpdate(values);
+            int i = mPager.getCurrentItem();
+
+            mPager.setCurrentItem(++i, true);
+            Log.d("TEST_", "na setitem " + i);
+            if (i> lengt) {
+                i = 0;
+                Log.d("TEST_", "in if "+ i);
+            }
+            else {
+                i++;
+                Log.d("TEST_", "in else " + i);
+            }
         }
 
         @Override
         protected Object doInBackground(Object[] params) {
-
+            timerStatus = true;
+            lengt = images.length;
+           // int i = 0;
             //loop
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            //express infinite loop gemaakt
+            do {
+
+                try {
+                    Thread.sleep(5000);
+                    //Log.d("TEST_", "voor setitem " + i);
+
+                    publishProgress();
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while (timerStatus = true);
             //end loop
 
             return null;
+        }
+
+        public void stopTimer()
+        {
+            timerStatus =false;
+        }
+
+        public void restartTimer()
+        {
+            timerStatus =true;
         }
     }
 
