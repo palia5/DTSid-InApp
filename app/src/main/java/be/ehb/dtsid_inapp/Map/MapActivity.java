4f68389d22dbd6f095f51dbdcf7ee3f3c9ee15af
tomnahooy <tom.nahooy@gmail.com>
2015-06-16 10:03:16 +0200

    Trying to update this branch

--- app/src/main/java/be/ehb/dtsid_inapp/Map/MapActivity.java
+++ app/src/main/java/be/ehb/dtsid_inapp/Map/MapActivity.java
@@ -15,35 +15,35 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 import be.ehb.dtsid_inapp.R;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+public class MapActivity extends Activity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mGoogleMap;
     private MapFragment mMapFragment;
     private Spinner mEventSpinner, mRegionSpinner;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Log.d("MapTest", "What the hell is going on?");
 
         mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
-       /*/ mEventSpinner = (Spinner) findViewById(R.id.sp_map_event);
-        mRegionSpinner = (Spinner) findViewById(R.id.sp_map_region);
-        */
+        /*/mEventSpinner = (Spinner) findViewById(R.id.sp_map_event);
+        mRegionSpinner = (Spinner) findViewById(R.id.sp_map_region); */
+
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
 
-        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(new LatLng(50.846777, 4.352360), 13);
-        mGoogleMap.animateCamera(cu);
+
 
     }
 
