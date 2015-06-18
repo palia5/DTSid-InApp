8db356e15efc8f5f666b4e3a33861733edffb2f6
DriesVanItterbeeck <driesvi@hotmail.com>
2015-06-18 15:15:50 +0200

    Drop tables fixed

--- app/src/main/java/be/ehb/dtsid_inapp/TeacherFragments/DepartmentLogin.java
+++ app/src/main/java/be/ehb/dtsid_inapp/TeacherFragments/DepartmentLogin.java
@@ -159,20 +159,20 @@ public class DepartmentLogin extends Fragment
 
     public void fillDB()
     {
-        if(!secret.equals(getStringFromSharedPrefs("SECRET")))
-        {
-            dbc.dropEventTable();
-            dbc.dropSchoolTable();
-            dbc.dropSubscriptionTable();
-            dbc.dropTeacherTable();
-        }
-
         if (!dbc.getAllSubscriptions().isEmpty() && secret.equals(getStringFromSharedPrefs("SECRET")))
         {
             everythingIsLoaded(true);
         }
         else if (!baseURL.equals("http://null"))
         {
+            if(!secret.equals(getStringFromSharedPrefs("SECRET")))
+            {
+                dbc.dropEventTable();
+                dbc.dropSchoolTable();
+                dbc.dropSubscriptionTable();
+                dbc.dropTeacherTable();
+            }
+
             putStringInSharedPrefs("BASEURL", baseURL);
             putStringInSharedPrefs("SECRET", secret);
             loadingDatabaseDialog.show();
